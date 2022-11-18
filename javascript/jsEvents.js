c9a2adb39fe375bd9147c2568eacca1a0b43329e
MadhuMuthumannan <madhu.muthumannan@gmail.com>
2022-11-18 07:21:35 +0530

    js events

---
+++ javascript/jsEvents.js
@@ -0,0 +1,11 @@
+function onBlur() {
+  console.log('triggered onblur');
+}
+
+function onChange() {
+  console.log('triggered onChange');
+}
+
+function onKeyPress() {
+  console.log('triggered on key press');
+}
