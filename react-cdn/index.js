dac4652e7fe074cff7f5d7509a841d912d30274d
MadhuMuthumannan <madhu.muthumannan@gmail.com>
2022-11-22 19:34:06 +0530

    added css

--- react-cdn/index.js
+++ react-cdn/index.js
@@ -1,8 +1,17 @@
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-  <div>
-    <h1>Hello World !</h1>
-    <span>Count : {1 + 2}</span>
-  </div>
+let counter = 0;
+const print = () => console.log(counter++);
+ReactDOM.createRoot(container).render(
+  6 > 2 ? (
+    <div>
+      <h1>Hello World !</h1>
+      <h1 className="blue">Hello World !</h1>
+      <span>{1 + 2}</span>
+      <button onClick={print}>Click</button>
+    </div>
+  ) : (
+    <div>
+      <span>Fall back !</span>
+    </div>
+  )
 );
