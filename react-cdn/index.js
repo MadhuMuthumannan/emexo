45ec89aa77cb12e8bdec8bec622671bcbd8fccd0
MadhuMuthumannan <madhu.muthumannan@gmail.com>
2022-11-22 18:57:03 +0530

    minor changes

--- react-cdn/index.js
+++ react-cdn/index.js
@@ -1,3 +1,8 @@
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<h1>Hello World !</h1>);
+root.render(
+  <div>
+    <h1>Hello World !</h1>
+    <span>Count : {1 + 2}</span>
+  </div>
+);
