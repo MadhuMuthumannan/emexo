c9893e9e62a982b1f60c614b75d12ddeab4aa58d
MadhuMuthumannan <madhu.muthumannan@gmail.com>
2022-12-06 20:36:15 +0530

    removed colon

--- react/my-app/src/children/userDetail.js
+++ react/my-app/src/children/userDetail.js
@@ -1,7 +1,7 @@
 function UserDetail({ user, updateUser }) {
   return (
     <div className="userDetailContainer">
-      <h3>User Detail Component:</h3>
+      <h3>User Detail Component</h3>
       {user.name} is of age {user.age}
       <button onClick={() => updateUser(user)}>Update User</button>
     </div>
