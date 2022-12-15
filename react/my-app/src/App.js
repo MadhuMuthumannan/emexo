bb1ccd7b05bd33fdc24f4733b85a76703cac2dfd
MadhuMuthumannan <madhu.muthumannan@gmail.com>
2022-12-15 16:12:49 +0530

    added css

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,12 +1,12 @@
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './App.css';
 export default function App() {
   return (
-    <>
-      <Link to="/displayCount">Display Count</Link>
-      <Link to="/userDetail">User Detail</Link>
-      <Link to="/listNames">List Names</Link>
-      <Link to="/listUsers">List Users</Link>
-    </>
+    <div>
+      <NavLink to="/displayCount">Display Count</NavLink>
+      <NavLink to="/userDetail">User Detail</NavLink>
+      <NavLink to="/listNames">List Names</NavLink>
+      <NavLink to="/listUsers">List Users</NavLink>
+    </div>
   );
 }
