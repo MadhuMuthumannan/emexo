9f4dd778b0e9af23b329ef8831a4dcfe84141b98
MadhuMuthumannan <madhu.muthumannan@gmail.com>
2022-12-21 20:43:00 +0530

    form related changes

--- react/my-app/src/children/form.js
+++ react/my-app/src/children/form.js
@@ -1,18 +1,15 @@
 import { useForm } from 'react-hook-form';
 import './form.css';
-import { ErrorMessage } from '@hookform/error-message';
 
 export default function Form() {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors },
   } = useForm();
-  console.log('something');
+
   const onSubmit = (data) => {
     console.log(data);
-    setError();
   };
 
   return (
@@ -34,13 +31,6 @@ export default function Form() {
               Name is required
             </p>
           )}
-          <ErrorMessage errors={errors} name="name" />
-
-          <ErrorMessage
-            errors={errors}
-            name="name"
-            render={({ message }) => <p>{message}</p>}
-          />
         </div>
         <div className="form-group">
           <label>Age:</label>
@@ -65,11 +55,30 @@ export default function Form() {
           )}
         </div>
         <div className="form-group">
+          <label>Email:</label>
+          <input
+            {...register('email', {
+              pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            })}
+            className="form-control"
+          />
+          {errors.email?.type === 'required' && (
+            <p role="alert" className="error-message">
+              Age is required
+            </p>
+          )}
+          {errors.email?.type === 'pattern' && (
+            <p role="alert" className="error-message">
+              Not a valid email
+            </p>
+          )}
+        </div>
+        <div className="form-group">
           <label>Gender:</label>
           <div className="form-check">
             <input
               className="form-check-input"
-              {...register('gender')}
+              {...register('gender', { required: true })}
               type="radio"
               value="male"
               id="male"
@@ -90,7 +99,6 @@ export default function Form() {
               Female
             </label>
           </div>
-
           {errors.gender?.type === 'required' && (
             <p role="alert" className="error-message">
               Gender is required
@@ -100,8 +108,9 @@ export default function Form() {
         <div className="form-group">
           <label>Rank:</label>
           <input
+            type="text"
             className="form-control"
-            {...register('rank', { required: true }, { min: 18, max: 99 })}
+            {...register('rank', { required: true }, { min: 1, max: 99 })}
           />
           {errors.rank?.type === 'required' && (
             <p role="alert" className="error-message">
@@ -110,12 +119,12 @@ export default function Form() {
           )}
           {errors.rank?.type === 'min' && (
             <p role="alert" className="error-messrank">
-              Min rank is 18
+              Min rank is 1
             </p>
           )}
           {errors.rank?.type === 'max' && (
             <p role="alert" className="error-message">
-              Max rank is 18
+              Max rank is 99
             </p>
           )}
         </div>
